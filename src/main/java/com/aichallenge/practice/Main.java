2b37a0e464ae7a339b712717631d740d695496bc
arcady ruvidic <liliasfhaleaf@gmail.com>
2023-03-10 10:36:12 +0100

    first implementation of initialization

--- src/main/java/com/aichallenge/practice/Main.java
+++ src/main/java/com/aichallenge/practice/Main.java
@@ -1,7 +1,21 @@
 package com.aichallenge.practice;
 
+import fr.emse.fayol.maqit.simulator.configuration.IniFile;
+import fr.emse.fayol.maqit.simulator.configuration.SimProperties;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            IniFile file = new IniFile("configuration.ini");
+            SimProperties properties = new SimProperties(file);
+            properties.initMQTT();
+            properties.displayParams();
+            properties.simulationParams();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+
+
     }
 }
