95b4f50ef8c8f7bf4893ecda2c487ffc7be236b7
LucasBonneau <lucas.bonneau@etu.emse.fr>
2023-04-05 11:28:48 +0200

    States added to the robot

---
+++ src/main/java/com/aichallenge/project/robot/RobotState.java
@@ -0,0 +1,7 @@
+package com.aichallenge.project.robot;
+
+public enum RobotState {
+    Active,
+    Standby,
+    Resting
+}
--- src/main/java/com/aichallenge/project/robot/StockRobot.java
+++ src/main/java/com/aichallenge/project/robot/StockRobot.java
@@ -1,14 +1,46 @@
 package com.aichallenge.project.robot;
 
+import com.aichallenge.project.item.Product;
 import fr.emse.fayol.maqit.simulator.robot.GridTurtlebot;
 
+
 public class StockRobot extends GridTurtlebot {
+
+    private Product order;
+    private RobotState robotState;
     public StockRobot(int id, String name, int field, int debug, int[] pos, int r, int c) {
         super(id, name, field, debug, pos, r, c);
+        order = null;
+        robotState = RobotState.Resting;
     }
-
     @Override
     public void move(int step) {
         this.moveForward();
+    }
+
+    public RobotState getState(){
+        return robotState;
+    }
+
+    /**
+     * The method sets the robotState to Active
+     * @param product product taken by the robot
+     */
+    public void feed(Product product){
+        if (this.order != null){
+            order = product;
+        }
+        robotState = RobotState.Active;
+    }
+
+    public Product deliver(){
+        Product orderToGive = order;
+        robotState = RobotState.Standby;
+        order = null;
+        return orderToGive;
+    }
+
+    public void rest(){
+        robotState = RobotState.Resting;
     }
 }
